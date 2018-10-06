275c7e5cfae5c2a28ff282020ffbc57efd76dc2e
Fredrik Sundberg <fredrik@sundbergmedia.com>
2018-10-06 02:52:29 +0200

    Add task for compiling pug templates

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var stylus = require('gulp-stylus');
+var pug = require('gulp-pug')
 var browserSync = require('browser-sync');
 var pkg = require('./package.json');
 var config = require('./config');
@@ -14,6 +15,17 @@ gulp.task('build-stylus', function() {
     .on('error', console.error.bind(console)) // swallow errors, so that simple syntax errors does not chrash the whole Gulp watch task
     .pipe(gulp.dest(config.path));
 });
+
+gulp.task('build-views', function buildHTML() {
+  return gulp.src('src/*.pug').pipe(pug({
+    compileDebug: true,
+    pretty: true,
+    verbose: true,
+    data: {
+      version: config.version,
+    }
+  })).pipe(gulp.dest(config.path))
+})
 
 gulp.task('browser-sync', function() {
   browserSync({
@@ -30,4 +42,5 @@ gulp.task('default', [
 
 gulp.task('watch', ['default'], function () {
   gulp.watch(config.paths.stylus, ['build-stylus']);
+  gulp.watch(config.paths.views, ['build-views']);
 });
